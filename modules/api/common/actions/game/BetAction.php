879bd3b415a9d3728b04020efd6cf9e2b30e014d
vladayson <vladaysin@gmail.com>
2020-07-13 18:06:28 +0300

    format

--- modules/api/common/actions/game/BetAction.php
+++ modules/api/common/actions/game/BetAction.php
@@ -4,6 +4,7 @@ namespace app\modules\api\common\actions\game;
 
 use app\models\Game;
 use app\models\GameUser;
+use Exception as ExceptionAlias;
 use Yii;
 use yii\db\Exception;
 use yii\helpers\Json;
@@ -20,28 +21,27 @@ class BetAction extends Action
      */
     public function run()
     {
+        $gameId = Yii::$app->request->post('game_id', 0);
+        $points = Json::decode(Yii::$app->request->post('points'), true);
 
-        $gameId =\Yii::$app->request->post('game_id');
-        $points =Json::decode(\Yii::$app->request->post('points'),true);
-
-            try {
-                $winPoints=[];
-                if(!$game=Game::find($gameId)->one()){
-                    \Yii::t('app',"Game is not found");
+        try {
+            $winPoints = [];
+            if (!$game = Game::findOne($gameId)) {
+                return $this->controller->onError(Yii::t('app', "Game is not found"));
+            }
+            if ($game->status == 1) {
+                $gameCombinations = $game->gameCombinations;
+                foreach ($gameCombinations as $k => $v) {
+                    array_push($winPoints, $v->point);
                 }
-                if( $game->status==1) {
-                    $gameCombinations = $game->gameCombinations;
-                    foreach ($gameCombinations as $k=>$v){
-                        array_push($winPoints,$v->point);
-                    }
-                    foreach ($points as $k=>$v) {
-                        $gameUser = new GameUser();
-                        $gameUser->game_id = $gameId;
-                        $gameUser->user_id = \Yii::$app->user->id;
-                        $gameUser->point = $v;
-                        $gameUser->date_point = date('Y-n-j G:i:s');
+                foreach ($points as $k => $v) {
+                    $gameUser = new GameUser();
+                    $gameUser->game_id = $gameId;
+                    $gameUser->user_id = Yii::$app->user->id;
+                    $gameUser->point = $v;
+                    $gameUser->date_point = date('Y-n-j G:i:s');
 
-                    $gameUser->is_correct =(in_array($v,$winPoints))?1:0;
+                    $gameUser->is_correct = (in_array($v, $winPoints)) ? 1 : 0;
                     if (!$gameUser->save()) {
                         throw new Exception("error with points");
                     }
@@ -49,7 +49,7 @@ class BetAction extends Action
             } else {
                 throw new Exception('game ended');
             }
-        } catch (\Exception $e) {
+        } catch (ExceptionAlias $e) {
             return $this->controller->onError($e->getMessage());
         }
 
