0615b6663e40041371f43efdbccac90b03b272bc
M.Petrov <mpetrov@dev-gns.com>
2020-07-16 11:50:50 +0300

    Add basic CRUD for User model

---
+++ models/UserSearch.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * UserSearch represents the model behind the search form of `app\models\User`.
+ */
+class UserSearch extends User
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['username', 'email', 'phone', 'password', 'token', 'date_token_expired', 'reset_password_token', 'date_reset_password'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = User::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'date_token_expired' => $this->date_token_expired,
+            'date_reset_password' => $this->date_reset_password,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'phone', $this->phone])
+            ->andFilterWhere(['like', 'password', $this->password])
+            ->andFilterWhere(['like', 'token', $this->token])
+            ->andFilterWhere(['like', 'reset_password_token', $this->reset_password_token]);
+
+        return $dataProvider;
+    }
+}
