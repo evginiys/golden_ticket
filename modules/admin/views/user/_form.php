f8ef54b4de9c795198a1c0b73703f64c39a6033b
M.Petrov <mpetrov@dev-gns.com>
2020-07-17 11:31:41 +0300

    Remove token field from user form

--- modules/admin/views/user/_form.php
+++ modules/admin/views/user/_form.php
@@ -20,8 +20,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'token')->textInput(['maxlength' => true]) ?>
-
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
