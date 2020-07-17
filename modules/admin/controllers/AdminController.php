7ce94a053b3d4cb2ae238586d4a809e31f83432e
M.Petrov <mpetrov@dev-gns.com>
2020-07-17 09:46:27 +0300

    Add base admin controller and access filter

---
+++ modules/admin/controllers/AdminController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\modules\admin\controllers;
+
+use app\models\User;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+/**
+ * Class AdminController
+ *
+ * @package app\modules\admin\controllers
+ */
+class AdminController extends Controller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => [User::ROLE_ADMIN]
+                    ]
+                ]
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ]);
+    }
+}
