d4c089dc9ecf9dacd93a84ee4c228bd64ee62a97
M.Petrov <mpetrov@dev-gns.com>
2020-07-22 11:33:25 +0300

    Require yii2-swagger extension

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,11 +5,24 @@ namespace app\controllers;
 use app\models\SignUpForm;
 use Yii;
 use yii\filters\AccessControl;
+use yii\helpers\Url;
 use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 
+/**
+ * Class SiteController
+ *
+ * @package app\controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="/",
+ *     produces={"application/json"},
+ *     consumes={"application/x-www-form-urlencoded"},
+ *     @SWG\Info(version="1.0", title="Simple API"),
+ * )
+ */
 class SiteController extends Controller
 {
     /**
@@ -44,6 +57,17 @@ class SiteController extends Controller
     public function actions()
     {
         return [
+            'docs' => [
+                'class' => 'yii2mod\swagger\SwaggerUIRenderer',
+                'restUrl' => Url::to(['site/json-schema']),
+            ],
+            'json-schema' => [
+                'class' => 'yii2mod\swagger\OpenAPIRenderer',
+                'scanDir' => [
+                    Yii::getAlias('@app/controllers'),
+                    Yii::getAlias('@app/models'),
+                ],
+            ],
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
