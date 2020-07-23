2252ae0cc464435c4bdb86e4800d14f34e3628bf
Lutsenko Evgenii <luthenkoev@gmail.com>
2020-07-23 12:14:24 +0300

    add ChangeUserInfAction

---
+++ modules/api/common/actions/ChangeUserInfAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\modules\api\common\actions;
+
+use Yii;
+use yii\rest\Action;
+
+/**
+ * Class changeUserInfAction
+ * @package app\modules\api\common\actions
+ */
+class changeUserInfAction extends Action
+{
+    /**
+     * @return array
+     */
+    public function run()
+    {
+        $user = Yii::$app->user->identity;
+        if (!empty($phone = Yii::$app->request->post('phone'))) {
+            $user->phone = $phone;
+        }
+        if (!empty($email = Yii::$app->request->post('email'))) {
+            $user->email = $email;
+        }
+        if (!empty($username = Yii::$app->request->post('username'))) {
+            $user->username = $username;
+        }
+        if (!$user->validate() or !$user->save()) {
+            return $this->controller->onError(Yii::t('app', $user->getErrors()));
+        }
+
+        return $this->controller->onSuccess($user->getAttributes(['username', 'phone', 'email']));
+    }
+}
