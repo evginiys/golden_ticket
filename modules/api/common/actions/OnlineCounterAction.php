7dce34e6c4b035554cef58c2bbaa1315a573bcfb
M.Petrov <mpetrov@dev-gns.com>
2020-07-29 12:38:54 +0300

    Add @swg docs for game actions: check, get-password, online-count

--- modules/api/common/actions/OnlineCounterAction.php
+++ modules/api/common/actions/OnlineCounterAction.php
@@ -8,6 +8,26 @@ use yii\rest\Action;
 /**
  * Class OnlineCounterAction
  * @package app\modules\api\common\actions
+ *
+ * @SWG\Get(path="/game/online-count",
+ *     tags={"Game"},
+ *     summary="Gets the count of currently online users.",
+ *     @SWG\Parameter(ref="#/parameters/authorization"),
+ *     @SWG\Response(
+ *         response=200,
+ *         description="Count of users online",
+ *         @SWG\Schema(
+ *             type="object",
+ *             @SWG\Property(property="error", type="integer", example=0),
+ *             @SWG\Property(
+ *                 property="data",
+ *                 type="object",
+ *                 @SWG\Property(property="count", type="integer", example=100)
+ *             )
+ *         )
+ *     ),
+ *     @SWG\Response(response=401, ref="#/responses/unauthorized")
+ * )
  */
 class OnlineCounterAction extends Action
 {
