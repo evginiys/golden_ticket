8999041675ca8a4907e02ba0ec1a5cb1e8b191fc
M.Petrov <mpetrov@dev-gns.com>
2020-07-30 11:56:35 +0300

    Add @swg docs for actions of DashboardController

--- modules/api/common/actions/dashboard/GetRateAction.php
+++ modules/api/common/actions/dashboard/GetRateAction.php
@@ -7,6 +7,27 @@ use yii\rest\Action;
 /**
  * Class GetRateAction
  * @package app\modules\api\common\actions\dashboard
+ *
+ * @SWG\Get(path="/deshboard/get-rate",
+ *     tags={"Dashboard"},
+ *     summary="Gets the currency exchange rate between the RUR and the coins.",
+ *     @SWG\Parameter(ref="#/parameters/authorization"),
+ *     @SWG\Response(
+ *         response=200,
+ *         description="Information about the exchange rate",
+ *         @SWG\Schema(
+ *             type="object",
+ *             @SWG\Property(property="error", type="integer", example=0),
+ *             @SWG\Property(
+ *                 property="data",
+ *                 type="object",
+ *                 @SWG\Property(property="rur_for_coins", type="number", example=100),
+ *                 @SWG\Property(property="coins_get", type="number", example=10)
+ *             )
+ *         )
+ *     ),
+ *     @SWG\Response(response=401, ref="#/responses/unauthorized")
+ * )
  */
 class GetRateAction extends Action
 {
@@ -18,6 +39,9 @@ class GetRateAction extends Action
 
     public function run()
     {
-        return $this->controller->onSuccess(['rur_for_coins' => self::RUR_GIVE_FOR_COINS, 'coins_get' => self::COINS_GET_BY_RUR]);
+        return $this->controller->onSuccess([
+            'rur_for_coins' => self::RUR_GIVE_FOR_COINS,
+            'coins_get' => self::COINS_GET_BY_RUR
+        ]);
     }
 }
--- modules/api/common/actions/dashboard/RefillAction.php
+++ modules/api/common/actions/dashboard/RefillAction.php
@@ -10,6 +10,34 @@ use yii\rest\Action;
 /**
  * Class RefillAction
  * @package app\modules\api\common\actions\dashboard
+ *
+ * @SWG\Post(path="/dashboard/refill",
+ *     tags={"Dashboard"},
+ *     summary="Refills the RUR wallet of current user.",
+ *     @SWG\Parameter(ref="#/parameters/authorization"),
+ *     @SWG\Parameter(
+ *         in="formData",
+ *         name="amount",
+ *         type="number",
+ *         required=true,
+ *         description="Amount of RUR to refill"
+ *     ),
+ *     @SWG\Response(
+ *         response=200,
+ *         description="Refill successful",
+ *         @SWG\Schema(
+ *             type="object",
+ *             @SWG\Property(property="error", type="integer", example=0),
+ *             @SWG\Property(property="data", type="boolean", example=true)
+ *         )
+ *     ),
+ *     @SWG\Response(
+ *         response=400,
+ *         description="Cannot refill wallet or incorrect amount value is provided",
+ *         @SWG\Schema(ref="#/definitions/ErrorResponse")
+ *     ),
+ *     @SWG\Response(response=401, ref="#/responses/unauthorized")
+ * )
  */
 class RefillAction extends Action
 {
